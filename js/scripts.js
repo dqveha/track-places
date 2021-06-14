e0e92af734e1c57df7b9d370dd2f2996432923fc
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-06-14 10:43:05 -0700

    Update business logic with addPlace and assignId methods.
    co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 //BUSINESS LOGIC
 
-//Constructor
+
+//B.L. for Records
+function Records() {
+  this.places = {};
+  this.currentId = 0;
+}
+
+Records.prototype.addPlace = function(place) {
+  place.id = this.assignId();
+  this.places[place.id] = place;
+};
+
+Records.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+
+//B.L. for TrackPlaces
 function TrackPlaces(location, landmarks, time, notes) {
   this.location = location;
   this.landmarks = landmarks;
